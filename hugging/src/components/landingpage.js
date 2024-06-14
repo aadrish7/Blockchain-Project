ad288c5e94c6a2f0a5aa5145048af4a1c6ab1d33
Muhammad Ali Hassan <120285932+AliHassanMirza1@users.noreply.github.com>
2024-06-14 21:53:54 +0500

    ImprovedUIPart1
    1

--- hugging/src/components/landingpage.js
+++ hugging/src/components/landingpage.js
@@ -1,3 +1,4 @@
+// LandingPage.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './landingpage.css';
@@ -5,9 +6,11 @@ import './landingpage.css';
 const LandingPage = () => {
   return (
     <div className="LandingPageContainer">
-      <h1>Welcome to Our Hugging Face Like Platform</h1>
-      <Link to="/login"><button className="Button">Login</button></Link>
-      <Link to="/signup"><button className="Button">Signup</button></Link>
+      <h1 className="AnimatedText">Welcome to Our Hugging Face Like Platform</h1>
+      <div className="ButtonContainer">
+        <Link to="/login"><button className="Button">Login</button></Link>
+        <Link to="/signup"><button className="Button">Signup</button></Link>
+      </div>
     </div>
   );
 };
