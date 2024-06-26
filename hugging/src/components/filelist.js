2a09f5bb4e3045dda9d34d723eab42a87f431636
Shaz-hash <73599731+Shaz-hash@users.noreply.github.com>
2024-06-26 15:46:01 +0500

    Update filelist.js

--- hugging/src/components/filelist.js
+++ hugging/src/components/filelist.js
@@ -329,10 +329,12 @@ function FileList() {
         {notification && <p class="notification-message" id="notificationMessage">{notification}</p>}
         {notification1 && <p class="notification-message" id="notificationMessage">{notification1}</p>}
         {notification2 && <p class="notification-message" id="notificationMessage">{notification2}</p>}
-        {notification3 && <p class="notification-message" id="notificationMessage">{notification3}</p>}
+        
         
         {notification5 && <p class="notification-message" id="notificationMessage">{notification5}</p>}
         {notification6 && <p class="notification-message" id="notificationMessage">{notification6}</p>}
+
+        {notification3 && <p class="notification-message" id="notificationMessage">{notification3}</p>}
         {notification7 && <p class="notification-message" id="notificationMessage">{notification7}</p>}
         
         {notification4 && <p class="notification-message" id="notificationMessage">{notification4}</p>}
